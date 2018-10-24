512709b1278ffd0c7710b4c6a544f7e5a854acb2
Kevin Orlando Vega Pérez <kevin.vega@globant.com>
2018-10-24 16:17:02 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+export default{
+    appName: "platzimusic",
+    apiKey: "55b5baa6a35e8720b4aeddb1921ee11a",
+    secret: "146a384087fda5736a450b493f3f3d09",
+    registeredTo: "kevinvega09"
+}
+
+
+
+
+//Application name	platzimusic
+//API key	55b5baa6a35e8720b4aeddb1921ee11a
+//Shared secret	146a384087fda5736a450b493f3f3d09
+//Registered to	kevinvega09
